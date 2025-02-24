9fd4256fecc8fb45a19134e973154b38e27add40
Sarfraz-Ahmad-it <sarfrazahmadiit@gmail.com>
2025-02-24 10:51:42 +0530

    Changes

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let inputBtn = document.querySelector('#dob');
 let calculateBtn = document.querySelector('#calBtn');
-let ageResult = document.querySelector('#result');
-
+let ageYear = document.querySelector('#year');
+let ageMonths = document.querySelector('#months');
+let ageDays = document.querySelector('#days');
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
 calculateBtn.addEventListener('click', function(){
     if(inputBtn.value == ""){
-        ageResult.innerHTML = "enter your date of birth";
+        alert('enter your date of birth');
+        
     }
     else{
-        console.log(inputBtn.value);
+        let dob = new Date(inputBtn.value);  
+        let now = new Date();
+        let years = now.getFullYear() - dob.getFullYear();
+        let months = now.getMonth() - dob.getMonth() +1 ;
+        let days = now.getDate() - dob.getDate();
+
+        ageYear.innerHTML = `Year = ${years}`;
+        ageMonths.innerHTML = `Months = ${months}`;
+        ageDays.innerHTML = `Days = ${days}`;
     }
 })
+
